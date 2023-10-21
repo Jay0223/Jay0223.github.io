2be7c147a44a9771be3c55139fb82f2a58dcfa96
JayW <fairjay8788@gmail.com>
2023-10-21 03:05:03 -0500

    minesweeper demo v0.8 playable

--- minesweeper/main.js
+++ minesweeper/main.js
@@ -1,9 +1,13 @@
 const controls = document.getElementById("controls");
 const minefield = document.getElementById("minefield");
 const cellsRevealed = new Set();
+const cellsFlagged = new Set();
 let minefieldSize = 100;
+let mineCount = 10;
+let gameLost = false;
 
-function createCells(size) {
+function initiateGame(size, count) {
+  // create cells
   const minefieldEdgeSize = Math.sqrt(size);
   minefield.style.setProperty(
     "grid",
@@ -14,47 +18,36 @@ function createCells(size) {
     newCell.classList.add("cell");
     newCell.dataset.id = i + 1;
     newCell.dataset.surroundingMineCount = 0;
+    newCell.dataset.status = "unrevealed";
     minefield.appendChild(newCell);
   }
-}
 
-function placeMines(size, mineCount) {
-  const mineLocations = new Set();
-  while (mineLocations.size < mineCount) {
+  // create random mine locations
+  const locations = new Set();
+  while (locations.size < count) {
     const newMine = Math.floor(Math.random() * size + 1);
-    mineLocations.add(newMine);
+    locations.add(newMine);
   }
-  const minefieldEdgeSize = Math.sqrt(size);
-  mineLocations.forEach((id) => {
+  locations.forEach((id) => {
     document.querySelector(`[data-id="${id}"]`).dataset.surroundingMineCount =
-      "mine";
+      "x";
     // update the surrounding-mine-count for cells around
     const cellsAround = getCellsAround(
       size,
       document.querySelector(`[data-id="${id}"]`)
     );
     cellsAround.forEach((cell) => {
-      if (cell.dataset.surroundingMineCount != "mine") {
+      if (cell.dataset.surroundingMineCount != "x") {
         cell.dataset.surroundingMineCount =
           +cell.dataset.surroundingMineCount + 1;
       }
     });
   });
+  return locations;
 }
 
-function fillNumbers() {
-  document.querySelectorAll(".cell").forEach((cell) => {
-    if (cell.dataset.surroundingMineCount === "mine") {
-      cell.textContent = "x";
-    } else if (+cell.dataset.surroundingMineCount > 0) {
-      cell.textContent = cell.dataset.surroundingMineCount;
-    }
-  });
-}
-
-createCells(minefieldSize);
-placeMines(minefieldSize, 10);
-fillNumbers();
+// initiate games and return mine locations (set)
+let mineLocations = initiateGame(minefieldSize, mineCount);
 
 // disable right click context menu in minefield
 minefield.addEventListener("contextmenu", (e) => {
@@ -63,15 +56,65 @@ minefield.addEventListener("contextmenu", (e) => {
 });
 
 minefield.addEventListener("mouseup", (e) => {
-  if (e.button === 0) {
-    console.log("left");
-  }
-  if (e.button === 2) {
-    console.log("right");
+  if (e.target.classList.contains("cell")) {
+    if (e.button === 0) {
+      revealCells(minefieldSize, e.target);
+      // need to check if game already lost by revealing a mine cell
+      if (
+        gameLost === false &&
+        cellsRevealed.size === minefieldSize - mineCount
+      ) {
+        // win by revealing all non-mine cells
+        console.log("WON!!!!!");
+      }
+    }
+    if (e.button === 2) {
+      flagCells(e.target);
+      if (
+        cellsFlagged.size === mineCount &&
+        [...cellsFlagged].every((id) => mineLocations.has(id))
+      ) {
+        // win by flagging all mines
+        console.log("WON!!!!!");
+      }
+    }
   }
 });
 
-function revealCells(cell) {}
+// using recursion, a function calling itself.
+function revealCells(size, cell) {
+  // the if condition makes sure recursion won't go infinite
+  if (!cellsRevealed.has(cell)) {
+    cellsRevealed.add(cell);
+    cell.textContent = cell.getAttribute("data-surrounding-mine-count");
+    cell.dataset.status = "revealed";
+    if (cell.dataset.surroundingMineCount === "x") {
+      gameLost = true;
+      console.log("game over");
+    } else if (+cell.dataset.surroundingMineCount === 0) {
+      cell.textContent = "";
+      const cellsAround = getCellsAround(size, cell);
+      cellsAround.forEach((c) => {
+        revealCells(size, c);
+      });
+    }
+  }
+}
+
+function flagCells(cell) {
+  if (cell.dataset.status === "unrevealed" && cellsFlagged.size < mineCount) {
+    cell.dataset.status = "flagged";
+    cell.textContent = "🚩";
+    cellsFlagged.add(+cell.dataset.id);
+  } else if (cell.dataset.status === "flagged") {
+    cell.dataset.status = "question";
+    cell.textContent = "?";
+    cellsFlagged.delete(+cell.dataset.id);
+  } else if (cell.dataset.status === "question") {
+    cell.dataset.status = "unrevealed";
+    cell.textContent = "";
+  }
+}
 
 function getCellsAround(size, cell) {
   const minefieldEdgeSize = Math.sqrt(size);
